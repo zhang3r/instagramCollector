65154c2069c3f35c9f329a907d4b1b203b1fbd7f
zhang3r <zhang3r@gatech.edu>
2015-09-20 20:28:36 -0400

    instagram call wiring

--- instagram_collector/collector/views.py
+++ instagram_collector/collector/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from .forms import TagForm
-
+from urllib.request import urlopen
+import json
 # Create your views here.
-def results (request, tagRequest_id):
-	response="tag response"
-	return HTTPResponse( response)
+def results (request):
+	response='result'
+	
+	return HttpResponse( response)
 
 def form(request):
 	if request.method == 'POST':
 		form = TagForm(request.POST)
 		if(form.is_valid()):
+			#TODO firing to instagram here
+			response = urlopen('https://api.instagram.com/v1/tags/'+form.cleaned_data['tag_name']+'/media/recent?client_id=b865ec47b91346f3a2cbcfe04a6a80d9')
+			content = response.readall()
+			jdata = json.loads(content.decode(encoding='utf-8' , errors='ignore'))
+			
+			print(str(content))
 			return HttpResponseRedirect('/results/')
 	else:
 		form = TagForm()
