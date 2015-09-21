cc65b3a9f0b3acaa2ae6c7a9621141869c557140
zhang3r <zhang3r@gatech.edu>
2015-09-20 22:16:48 -0400

    rm global var

--- instagram_collector/collector/views.py
+++ instagram_collector/collector/views.py
@@ -5,7 +5,7 @@ from .forms import TagForm
 from urllib.request import urlopen
 import json
 
-data =''
+
 # Create your views here.
 def results (request):
 	response=request.session.get('json_data')
@@ -15,12 +15,12 @@ def results (request):
 def form(request):
 	if request.method == 'POST':
 		form = TagForm(request.POST)
+		#TODO form field validation
 		if(form.is_valid()):
-			#TODO firing to instagram here
+			#TODO Authentication
 			response = urlopen('https://api.instagram.com/v1/tags/'+form.cleaned_data['tag_name']+'/media/recent?client_id=b865ec47b91346f3a2cbcfe04a6a80d9')
 			content = response.readall()
 			jdata = json.loads(content.decode(encoding='utf-8' , errors='ignore'))
-			global data
 			request.session['json_data'] =str(content)
 			
 			return HttpResponseRedirect('/results/')
