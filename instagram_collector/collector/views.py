222e4b432f5e713a8b16787886c6d50e0b2537c1
zhang3r <zhang3r@gatech.edu>
2015-09-20 21:36:19 -0400

    storing json data to session

--- instagram_collector/collector/views.py
+++ instagram_collector/collector/views.py
@@ -4,9 +4,11 @@ from django.http import HttpResponse
 from .forms import TagForm
 from urllib.request import urlopen
 import json
+
+data =''
 # Create your views here.
 def results (request):
-	response='result'
+	response=request.session.get('json_data')
 	
 	return HttpResponse( response)
 
@@ -18,8 +20,9 @@ def form(request):
 			response = urlopen('https://api.instagram.com/v1/tags/'+form.cleaned_data['tag_name']+'/media/recent?client_id=b865ec47b91346f3a2cbcfe04a6a80d9')
 			content = response.readall()
 			jdata = json.loads(content.decode(encoding='utf-8' , errors='ignore'))
+			global data
+			request.session['json_data'] =str(content)
 			
-			print(str(content))
 			return HttpResponseRedirect('/results/')
 	else:
 		form = TagForm()
