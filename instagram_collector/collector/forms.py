d53d335ce443566ebaba614d1fe0854acc0a4efc
zhang3r <zhang3r@gatech.edu>
2015-09-20 22:44:25 -0400

    date field validation

--- instagram_collector/collector/forms.py
+++ instagram_collector/collector/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.core.exceptions import ValidationError
 import datetime
 
 class TagForm(forms.Form):
@@ -7,3 +7,9 @@ class TagForm(forms.Form):
 	tag_name = forms.CharField(required=True,label='enter tags separated by ",":', max_length=256)
 	start_date = forms.DateField(required=True)
 	end_date = forms.DateField(required=True)
+	def clean(self):
+		if self.cleaned_data.get('start_date') > self.cleaned_data.get('end_date'):
+			raise ValidationError("end date must be greater than start date")
+		if self.cleaned_data.get('end_date')>datetime.date.today():
+			raise ValidationError("end date cannot be in the future")
+		return self.cleaned_data
--- instagram_collector/collector/views.py
+++ instagram_collector/collector/views.py
@@ -15,14 +15,22 @@ def results (request):
 def form(request):
 	if request.method == 'POST':
 		form = TagForm(request.POST)
-		#TODO form field validation
+		
 		if(form.is_valid()):
+			startDate = form.cleaned_data['start_date']
+			endDate = form.cleaned_data['end_date']
+
 			#TODO Authentication
 			response = urlopen('https://api.instagram.com/v1/tags/'+form.cleaned_data['tag_name']+'/media/recent?client_id=b865ec47b91346f3a2cbcfe04a6a80d9')
 			content = response.readall()
 			jdata = json.loads(content.decode(encoding='utf-8' , errors='ignore'))
+			# go through data find min/max tag id
+			# find min/max time stamp
+			# guesstimate min/max tag id that correspond with date
+			# while current max tag <max tag
+			# call service and add to jsondata
 			request.session['json_data'] =str(content)
-			
+			# Store in DB
 			return HttpResponseRedirect('/results/')
 	else:
 		form = TagForm()
