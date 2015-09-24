dae73f723364db7ffab10e8b46b37fbc87a66968
zhang3r <zhang3r@gatech.edu>
2015-09-24 00:15:11 -0400

    postgres impl

--- instagram_collector/collector/views.py
+++ instagram_collector/collector/views.py
@@ -5,16 +5,20 @@ from .forms import TagForm
 from urllib.request import urlopen
 import datetime
 import json
-import pymongo
-from pymongo import MongoClient
+import psycopg2
 import time
+from psycopg2.extras import Json
+
 
 def __saveToDB(jsondata):
-		client = MongoClient('localhost',27017)
-		db = client['my_db']
-		collection = db['pixlee']
-		jsonID = db['pixlee'].insert_one(jsondata).inserted_id
-		client.close()
+	#try:
+		conn = psycopg2.connect("host='localhost' dbname='mydb' user='Zhang3r' password='password'")
+		cur = conn.cursor()
+	
+		cur.execute("insert into myData (data) values (%s)",[Json(jsondata)])
+		conn.close()
+	#except:
+	#	raise ValueError('error connecting to db.')
 # Create your views here.
 def results (request):
 	response=request.session.get('json_data')
@@ -41,8 +45,8 @@ def form(request):
 			minTime=datetime.datetime.today()
 			maxTime=datetime.datetime.fromtimestamp(0)
 			for post in jdata['data'][:]:
-				minTime= min(datetime.datetime.fromtimestamp(int(post['caption']['created_time'])//1000.0), minTime)
-				maxTime= max(datetime.datetime.fromtimestamp(int(post['caption']['created_time'])//1000.0), maxTime)
+				minTime= min(datetime.datetime.fromtimestamp(int(post['caption']['created_time'])), minTime)
+				maxTime= max(datetime.datetime.fromtimestamp(int(post['caption']['created_time'])), maxTime)
 			# guesstimate min/max tag id that correspond with date
 			# difference between time / difference between tag, results in tag/time
 			tag_diff = min_tag - max_tag
@@ -52,18 +56,19 @@ def form(request):
 			time_diff =  maxTimeSeconds- minTimeSeconds if maxTimeSeconds- minTimeSeconds > 0 else 1
 			# calculate the difference of min tag based on start date and today using tag/time
 
-			tag_rate = tag_diff//time_diff
-			target_start_tag = (maxTime-startDate).total_seconds()*tag_rate
-			target_end_tag = (minTime-endDate).total_seconds()*tag_rate
+			tag_rate = tag_diff//time_diff # tags/second
+			target_start_tag = max_tag-(abs(maxTime-startDate).total_seconds()*tag_rate)
+			target_end_tag = max_tag-(abs(minTime-endDate).total_seconds()*tag_rate)
+			
 			# while current max tag <max tag
 			while( target_start_tag<target_end_tag):
-				tempResponse = urlopen('https://api.instagram.com/v1/tags/'+form.cleaned_data['tag_name']+'/media/recent?client_id=b865ec47b91346f3a2cbcfe04a6a80d9&max_tag_id='+target_start_tag)
+				tempResponse = urlopen('https://api.instagram.com/v1/tags/'+form.cleaned_data['tag_name']+'/media/recent?client_id=b865ec47b91346f3a2cbcfe04a6a80d9&max_tag_id='+str(target_start_tag))
 				tempContent = tempResponse.readall()
 				tempData = json.loads(content.decode(encoding='utf-8' , errors='ignore'))
 				# call service and add to jsondata
-				data+=tempContent
-				result+=str(tempContent)
-				target_start_tag=int(tempContent['pagination']['next_max_tag_id'])
+				data.update(tempData)
+				result+=str(tempData)
+				target_start_tag=int(tempData['pagination']['next_max_tag_id'])
 			request.session['json_data'] =result
 			__saveToDB(data)
 			# Store in DB
